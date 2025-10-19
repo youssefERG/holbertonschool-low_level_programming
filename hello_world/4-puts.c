50cd6d1e441e8ad13d8ec7d041b2547cef5f5094
youssefERG <erengamer555@gmail.com>
2025-10-19 19:55:23 +0200

    fixing

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**main - prints the message
+*followed by new line
+*
+*return 0
+*/
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
---
+++ hello_world/main.c
@@ -0,0 +1,1 @@
+#include <stdio.h>\nint main(void){printf("Hello\n");return 0;}
