8b92772171a247ebd5e87bd9dd23b12b407cb365
youssefERG <erengamer555@gmail.com>
2025-10-19 20:00:52 +0200

    can this task end

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-
-/**main - prints the message
-*followed by new line
-*
-*return 0
-*/
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle"
+ * followed by a new line
+ *
+ * Return: 0
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
