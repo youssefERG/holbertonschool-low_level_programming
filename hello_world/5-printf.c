50450ac35d5e5e2cd5ddbbd91479ccca475a5ade
youssefERG <erengamer555@gmail.com>
2025-10-19 20:21:21 +0200

    auto push

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints exactly "with proper grammar, but the outcome is a piece of art,"
- * 
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
 
 
