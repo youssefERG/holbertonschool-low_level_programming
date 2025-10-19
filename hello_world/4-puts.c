45ec88990245f42f99199cdc5e520574fc9fc482
youssefERG <erengamer555@gmail.com>
2025-10-19 20:05:35 +0200

    ...

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-/**
- * main - prints exactly "Programming is like building a multilingual puzzle"
- * followed by a new line
- *
- * Return: 0
- */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
