ea1f62200df6690e8e663a3785c478378db97e19
youssefERG <erengamer555@gmail.com>
2025-10-19 20:10:55 +0200

    auto push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle"
- * followed by a new line
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
 
 
 
