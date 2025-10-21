5c6c6b8b82e21665e72c2905412a93a7decdd4c7
youssefERG <erengamer555@gmail.com>
2025-10-21 11:59:03 +0200

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar("\n");
+
+	return (0);
+}
+
