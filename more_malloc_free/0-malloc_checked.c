7f38969053d0d53c552b5f3361756af19032f0df
youssefERG <erengamer555@gmail.com>
2025-11-08 18:04:51 +0100

    more malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ *         exits with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *p;
+
+    p = malloc(b);
+    if (p == NULL)
+        exit(98);
+    return (p);
+}
