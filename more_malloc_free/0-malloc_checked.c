0521ee6b12f57b862ab8babfde1f30d3d3fce96d
youssefERG <erengamer555@gmail.com>
2025-11-09 15:31:53 +0100

    f b

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,6 @@
  * @b: number of bytes to allocate
  *
  * Return: pointer to the allocated memory
- *         exits with status 98 if malloc fails
  */
 void *malloc_checked(unsigned int b)
 {
@@ -14,5 +13,6 @@ void *malloc_checked(unsigned int b)
     p = malloc(b);
     if (p == NULL)
         exit(98);
+
     return (p);
 }
