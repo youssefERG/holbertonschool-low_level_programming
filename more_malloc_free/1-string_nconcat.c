42ab77fda9c9083f41d67a6b73d6f83e4824f045
youssefERG <erengamer555@gmail.com>
2025-11-09 15:33:52 +0100

    wtv

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, j, len1 = 0, len2 = 0;
-    char *ptr;
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *ptr;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    ptr = malloc(len1 + n + 1);
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc(len1 + n + 1);
+	if (ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        ptr[i] = s1[i];
-    for (j = 0; j < n; j++)
-        ptr[i + j] = s2[j];
-    ptr[i + j] = '\0';
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
-    return (ptr);
+	return (ptr);
 }
