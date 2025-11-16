0435a4327b9bfb7939e71ac8fe5d84272021e95b
youssefERG <erengamer555@gmail.com>
2025-11-16 21:34:42 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,31 +3,45 @@
 #include <stdarg.h>
 
 /**
- * print_strings - prints strings with a separator
- * @separator: string printed between strings
- * @n: number of strings passed
+ * print_all - prints anything based on format string
+ * @format: string of types (c = char, i = int, f = float, s = string)
  *
  * Return: void
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i;
+	unsigned int i = 0;
 	char *str;
+	char *separator = "";
 
-	va_start(args, n);
+	va_start(args, format);
 
-	for (i = 0; i < n; i++)
+	while (format && format[i])
 	{
-		str = va_arg(args, char *);
-
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-
-		if (separator != NULL && i != n - 1)
-			printf("%s", separator);
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
 	}
 
 	printf("\n");
