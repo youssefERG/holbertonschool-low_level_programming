8bfd459d158fa0133e56d38e247c73edb9c1e22e
youssefERG <erengamer555@gmail.com>
2025-11-26 10:53:16 +0100

    nvm

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,54 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of the string, or 0 if NULL
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @s: string to duplicate
+ *
+ * Return: pointer to new string, or NULL on failure
+ */
+char *_strdup(const char *s)
+{
+	unsigned int len;
+	char *copy;
+	unsigned int i;
+
+	if (s == NULL)
+		return (NULL);
+
+	len = _strlen(s);
+	copy = malloc(len + 1);
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = s[i];
+
+	copy[len] = '\0';
+
+	return (copy);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: double pointer to the head of the list
  * @str: string to store in the new node (will be duplicated)
