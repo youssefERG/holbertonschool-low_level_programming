2336eb0d342b87c4581fefe4cf258ca722c2e0ec
youssefERG <erengamer555@gmail.com>
2025-11-26 10:47:44 +0100

    ..

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,28 +1,57 @@
 #include "lists.h"
-/* minimal _strlen and _strdup implementations (non-static) */
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of the string, or 0 if NULL
+ */
 unsigned int _strlen(const char *s)
 {
-    unsigned int i = 0;
-    if (!s) return (0);
-    while (s[i]) i++;
-    return (i);
+	unsigned int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i])
+		i++;
+
+	return (i);
 }
 
+/**
+ * _strdup - duplicates a string
+ * @s: string to duplicate
+ *
+ * Return: pointer to new string, or NULL on failure
+ */
 char *_strdup(const char *s)
 {
-    unsigned int len = _strlen(s);
-    char *copy = malloc(len + 1);
-    unsigned int i;
-    if (!copy) return (NULL);
-    for (i = 0; i < len; i++)
-        copy[i] = s[i];
-    copy[len] = '\0';
-    return (copy);
+	unsigned int len;
+	char *copy;
+	unsigned int i;
+
+	if (s == NULL)
+		return (NULL);
+
+	len = _strlen(s);
+	copy = malloc(len + 1);
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = s[i];
+
+	copy[len] = '\0';
+
+	return (copy);
 }
+
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the head of the list
- * @str: string to store in the new node (will be duplicated)
+ * @str: string to store in the new node
  *
  * Return: address of the new element, or NULL on failure
  */
@@ -35,6 +64,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	dup = _strdup(str);
+
 	if (str != NULL && dup == NULL)
 		return (NULL);
 
